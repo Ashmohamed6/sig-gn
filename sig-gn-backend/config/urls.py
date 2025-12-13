c8ea5fba2a6d838fc749093f6732f546d521cd5d
Sedjro <manwenselove@gmail.com>
2025-12-13 11:12:35 +0100

    fix:css non charé en mode debug

--- sig-gn-backend/config/urls.py
+++ sig-gn-backend/config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -19,3 +21,8 @@ urlpatterns = [
     path("api/data/", include("data_api.urls")),
     path("api/admin-core/", include("admin_core.urls")),
 ]
+
+
+# Servir les fichiers statiques en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
